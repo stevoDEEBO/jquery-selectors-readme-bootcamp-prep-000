5c99a0b5039b763df94a2cc67d0b9de11ba1a13c
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-08 03:49:22 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,20 @@
 // declare your functions here...
 function paragraphSelector() {
-  
+return $('p')
 }
 
 function lastImageSelector() {
-  
+
 }
 
 function ninjaBabySelector() {
-  
+
 }
 
 function divSelector() {
-  
+
 }
 
 function firstListItem() {
-  
+
 }

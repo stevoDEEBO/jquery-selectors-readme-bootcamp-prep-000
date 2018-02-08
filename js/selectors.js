813e897fc014de74829390c597479fee6d272711
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-08 03:59:39 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -16,5 +16,5 @@ function divSelector() {
 }
 
 function firstListItem() {
-  return $('div ul img:first-child')//$('.pics  #pic-list:first-child')
+  return $('div ul li img:first-child')//$('.pics  #pic-list:first-child')
 }
